f62359c68d1ca7d60619a831afeaaba7d6bfd66c
alimarchi <marchi_alice@yahoo.com>
2023-05-02 15:17:28 +0200

    starting new version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
+import { useState } from "react";
+import ReactDOM from "react-dom";
 import classes from "./App.module.css";
 import Search from "./components/search/Search";
 import CurrentWeather from "./components/current-weather/CurrentWeather";
 import Forecast from "./components/forecast/Forecast";
-import { WEATHER_API_KEY, WEATHER_API_URL } from "./api";
-import { useState } from "react";
+import {  WEATHER_API_URL } from "./api";
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
+  const [visible, setVisible] = useState(true);
 
   const handleOnSearchChange = (searchData) => {
     // In my object I have a value property that contains the lat and lon, as a string, separated by a space
     const [lat, lon] = searchData.value.split(" ");
 
     const currentWeatherFetch = fetch(
-      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`
     );
     const forecastFetch = fetch(
-      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`
     );
 
     Promise.all([currentWeatherFetch, forecastFetch])
@@ -35,11 +37,15 @@ function App() {
   console.log(forecast);
 
   return (
-    <div className={classes.container}>
-      <Search onSearchChange={handleOnSearchChange} />
-      {currentWeather && <CurrentWeather data={currentWeather} />}
-      {forecast && <Forecast data={forecast} />}
-    </div>
+    <>
+      {ReactDOM.createPortal(<Search onSearchChange={handleOnSearchChange} visible={visible} />, document.querySelector("#modal"))}
+    </>
+    
+    // <div className={classes.container}>
+    //   <Search onSearchChange={handleOnSearchChange} />
+    //   {currentWeather && <CurrentWeather data={currentWeather} />}
+    //   {forecast && <Forecast data={forecast} />}
+    // </div>
   );
 }
 
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -4,48 +4,58 @@ import { GEO_API_URL, geoApiOptions } from "../../api";
 import classes from "./Search.module.css";
 
 function Search({ onSearchChange }) {
-  const [search, setSearch] = useState(null);
+  const [search, setSearch] = useState("");
+  const [error, setError] = useState(null)
 
-  const loadOptions = (inputValue) => {
-    // I fetch the data from the api. I use the inputValue to search for cities that start with the letters that the user typed in the search bar and I only return cities with a population of 2000 or more.
-    return fetch(
-      `${GEO_API_URL}/cities?minPopulation=2000&namePrefix=${inputValue}`,
-      geoApiOptions
+  const handleOnChange = (event) => {
+    // set as search value what the user types
+    const searchValue = event.target.value;
+    setSearch(searchValue);
+  };
+
+  const handleSearch = () => {
+    // fetch the data from the api using searchValue
+    const searchValue = search.trim()
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${searchValue}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
     )
       .then((response) => response.json())
       .then((response) => {
-        // it returns an array of objects. Each object represents a city. I map over the array and return an object with the value and label properties. The value property is the latitude and longitude of the city, I need them for the weather api I'm going to use. The label property is the name of the city and the country code.
-        return {
-          options: response.data.map((city) => {
-            return {
-              value: `${city.latitude} ${city.longitude}`,
-              label: `${city.name}, ${city.countryCode}`,
-            };
-          }),
-        };
+        if (response.length > 0) {
+          handleOnSelect(`${response[0].lat} ${response[0].lat}`)
+          
+        } else {
+          setError("City not found. Please try again!")
+          setTimeout(() => {
+            setError(null)
+          }, 2000);
+        }
       })
-      .catch((err) => console.error(err));
-  };
+      .catch((error) => console.error(error));
+  }
 
-  // handleOnChange is a function that is called when the user selects a city from the dropdown list.
-  const handleOnChange = (searchData) => {
-    setSearch(searchData);
-    // onSearchChange is a function that is passed down from the parent component. It is called when the user selects a city from the dropdown list.
-    onSearchChange(searchData);
+  const handleOnSelect = (selectedOption) => {
+    console.log(selectedOption)
+    onSearchChange(selectedOption);
   };
 
   return (
-    // asyncPaginate is a component that I imported from a library called react-select-async-paginate. It allows you to search for a city and then select it.
-    <AsyncPaginate
-      placeholder="Search for a city"
-      // debounceTimeout is the amount of time in milliseconds that the user has to stop typing before the search is executed.
-      debounceTimeout={600}
-      value={search}
-      onChange={handleOnChange}
-      // we use an api to search for cities. The api is called when the user types in the search bar.
-      loadOptions={loadOptions}
-      className={classes.search}
-    />
+    <div className={classes.modal}>
+      <div className={classes.header}>
+        <h2>My Weather App</h2>
+      </div>
+      <div className={classes["input-container"]}>
+        {error !== null && <div className={classes.error}>{error}</div>}
+        <input
+          type="text"
+          placeholder="Search for a city..."
+          className={classes.input}
+          value={search}
+          onChange={handleOnChange}
+        ></input>
+        <button className={classes["search-button"]} onClick={handleSearch}>SEARCH</button>
+      </div>
+    </div>
   );
 }
 
